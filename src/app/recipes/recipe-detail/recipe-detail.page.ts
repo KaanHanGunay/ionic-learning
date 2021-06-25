87293ced31cb6ff75b8f9272439804880b69d3b8
Kaan Han Günay <kaangunay16@gmail.com>
2021-06-25 10:25:51 +0300

    Alert

--- src/app/recipes/recipe-detail/recipe-detail.page.ts
+++ src/app/recipes/recipe-detail/recipe-detail.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { RecipesService } from '../recipes.service';
 import { Recipe } from '../recipe.model';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -11,7 +12,12 @@ import { Recipe } from '../recipe.model';
 export class RecipeDetailPage implements OnInit {
   loadedRecipe?: Recipe;
 
-  constructor(private activatedRoute: ActivatedRoute, private recipeService: RecipesService) { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private recipeService: RecipesService,
+    private router: Router,
+    private alertController: AlertController
+  ) { }
 
   ngOnInit() {
     this.activatedRoute.paramMap.subscribe(paramMap => {
@@ -24,4 +30,23 @@ export class RecipeDetailPage implements OnInit {
     });
   }
 
+  onDeleteRecipe(): void {
+    this.alertController.create({
+      header: 'Are you sure?',
+      message: 'Do you want to delete this?',
+      buttons: [{
+          text: 'Cancel',
+          role: 'cancel'
+        }, {
+          text: 'Delete',
+          handler: () => {
+            this.recipeService.deleteRecipe(this.loadedRecipe.id);
+            this.router.navigate(['/recipes']);
+          }
+        }
+      ]
+    }).then(alertElement => {
+      alertElement.present();
+    });
+  }
 }
